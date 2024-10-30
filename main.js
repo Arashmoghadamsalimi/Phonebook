2eec53a796d1bffb140e85c25a7899e12d64771c
arashmogh <arash.ms1994@gmail.com>
2024-10-30 23:15:09 +0330

    third

--- main.js
+++ main.js
@@ -2,22 +2,24 @@ import { data } from "autoprefixer";
 import "./style.css";
 
 let contacts = [];
-async function render() {
 
-  try {
-    const response = await fetch(
-      "https://671cf16209103098807bb538.mockapi.io/Users"
-    );
-    contacts = await response.json();
-    contactsAmounts();
-    contacts.forEach(showContact);
-    contacts.forEach(showNumber);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-  console.log(contacts)
+function createContact() {
+  let addContact = document.getElementById("createContact");
+  addContact.addEventListener("click", (e) => {
+    let firstName = prompt("Enter first name");
+    let lastName = prompt("Enter last name");
+    let number = +prompt("Enter number");
+    let contact = {
+      firstName,
+      lastName,
+      number,
+    };
+    contacts.push(contact);
+    console.log(contacts);
+    localStorage.setItem("contacts", contacts);
+  });
 }
-render();
+createContact();
 
 let contactsAmounts = () => {
   if (contacts.length == 0) {
@@ -30,30 +32,34 @@ let contactsAmounts = () => {
 function showContact(contact) {
   const firstName = document.createElement("p");
   const lastName = document.createElement("p");
-  firstName.innerText = `${contact.FirsName}`;
-  lastName.innerText = `${contact.LastName}`;
+  const number = document.createElement("p");
+  firstName.innerText = `${contact.firstName}`;
+  lastName.innerText = `${contact.lastName}`;
+  number.innerText = `${contact.number}`;
   document.getElementById("contact_firstName").appendChild(firstName);
   document.getElementById("contact_lastName").appendChild(lastName);
-}
-
-function showNumber(contact) {
-  const number = document.createElement("p");
-  number.innerText = `${contact.Number}`;
   document.getElementById("contact_number").appendChild(number);
 }
 
+// function search() {
+//   let query = document.getElementById("search")
+//   let matchingContact = contacts.filter((contact) => {
+//     let firstName = contact.firstName.tolowercase();
+//     let lastName = contact.lastName.tolowercase();
+//     let number = contact.number;
+    
+//     return (
+//       firstName.includes(query) ||
+//       lastName.includes(query) ||
+//       number.includes(query) ||
+//     );
+//   });
+// }
+// search()
 
-// save in local storage
-
-console.log(contacts)
-function createContact() {
-  let addContact = document.getElementById("createContact");
-  addContact.addEventListener("click", (e) => {
-    let firstName = prompt("Enter first name");
-    let lastName = prompt("Enter last name");
-    let number = prompt("Enter number");
-    localStorage.setItem(firstName+lastName, number);
-  console.log(addContact);
-  });
+function render() {
+  localStorage.getItem("contacts", contacts);
+  contactsAmounts();
+  contacts.forEach(showContact);
 }
-createContact();
+render();
